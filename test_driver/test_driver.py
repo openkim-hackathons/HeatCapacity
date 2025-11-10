82dd163a611cbae5490ed5109d7e05924e88768e
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2025-11-10 17:11:31 -0500

    Added README

--- test_driver/test_driver.py
+++ test_driver/test_driver.py
@@ -27,25 +27,26 @@ class TestDriver(SingleCrystalTestDriver):
         is more accurate than fluctuation-based approaches for computing heat capacities from molecular-dynamics
         simulations.
 
-        The finite-difference approach requires to run at least three molecular-dynamics simulations at different
-        temperatures, one at the target temperature, one at a slightly lower temperature and one at a slightly higher
-        temperature.  It is possible to add more temperature points symmetrically around the target temperature for
-        higher-order finite-difference schemes.
+        The finite-difference approach requires to run at least three molecular-dynamics simulations with a fixed number
+        of atoms N at constant pressure (P) and at different constant temperatures T (NPT ensemble), one at the target
+        temperature at which the heat capacity and thermal expansion tensor are to be estimated, one at a slightly lower
+        temperature, and one at a slightly higher temperature. It is possible to add more temperature points
+        symmetrically around the target temperature for higher-order finite-difference schemes.
 
-        This test driver repeats the unit cell to build a supercell and then runs molecular-dynamics simulations in the
-        NPT ensemble using Lammps.
+        This test driver repeats the unit cell of the zero-temperature crystal structure to build a supercell and then
+        runs molecular-dynamics simulations in the NPT ensemble using Lammps.
 
-        This test driver uses kim_convergence to detect an equilibrated molecular-dynamics simulation. It checks
+        This test driver uses kim_convergence to detect equilibrated molecular-dynamics simulations. It checks for the
         convergence of the volume, temperature, enthalpy and cell shape parameters every 10000 timesteps.
 
-        After the molecular-dynamics simulations, the symmetry of the final structures are checked to ensure that they
-        did not change.
+        After the molecular-dynamics simulations, the symmetry of the average structures during the equilbrated parts of
+        the runs are checked to ensure that they did not change in comparison to the initial structure. Also, it is
+        ensured that replicated atoms in replicated unit atoms are not too far away from the average atomic positions.
 
-        The crystals might melt or vaporize during the simulations. In that case, kim_convergence would only detect
-        equilibration after an unnecessarily long simulation. Therefore, we initially check for melting or vaporization
-        during a short initial simulation of 5000 timesteps. During this run, we monitor the mean-squared displacement
-        (MSD) of atoms during the simulation. If the MSD exceeds a given threshold value (msd_threshold), an error is
-        raised.
+        The crystals might melt or vaporize during the simulations. In that case, kim-convergence would only detect
+        equilibration after unnecessarily long simulations. Therefore, this test driver initially check for melting or
+        vaporization during short initial simulations. During these initial runs, the mean-squared displacement (MSD) of
+        atoms during the simulations is monitored. If the MSD exceeds a given threshold value, an error is raised.
 
         All output files are written to the "output" directory.
 
